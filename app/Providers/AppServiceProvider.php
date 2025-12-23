a242e6378c79ed54234d45b9a5df7c8bd43c9a39
PedroRenatoES <pedrorenatoes@gmail.com>
2025-12-23 14:41:25 -0400

    Configuracion de https corregida

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -19,6 +19,11 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        // Force HTTPS when behind a reverse proxy with HTTPS
+        if (isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] === 'https') {
+            \Illuminate\Support\Facades\URL::forceScheme('https');
+        }
+        
         // Force HTTPS in production environment
         if ($this->app->environment('production')) {
             \Illuminate\Support\Facades\URL::forceScheme('https');
